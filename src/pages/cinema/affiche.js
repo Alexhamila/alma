d608b7c6d53b4f7424112df57e2aa55de87d2477
Alexhamila <alexhamiladeveloper@gmail.com>
2020-03-08 01:15:22 +0100

    Update

--- src/pages/cinema/affiche.js
+++ src/pages/cinema/affiche.js
@@ -6,10 +6,6 @@ import { graphql } from "gatsby"
 import "../../components/cinema/index.css"
 import HeaderCinema from "../../components/headercinema"
 
-var imgUrl_1 = "/images/158085093460089227.png";
-const img_1 = {
-  backgroundImage: 'url("' + imgUrl_1 + '")'
-};
 var imgUrl_2 = "/images/158090684095061065.png";
 const img_2 = {
   backgroundImage: 'url("' + imgUrl_2 + '")'
@@ -30,9 +26,21 @@ const AfficheCinema = ({ data }) =>  {
     <Layout header="cinema">
         <SEO title="Accueil"></SEO>
         <div className="cinema">
+          
+           {data.allStrapiAffiche.edges.map((document, index) => {
+
+              console.log(index);
+              if(index == 0){
+
+              var imgUrl_1 = document.node.Affiche.publicURL;
+
+              const img_1 = {
+                backgroundImage: 'url("' + imgUrl_1 + '")'
+              };
+             return(
             <div className="background-cinema" style={background_gradient}>
             <div className="background-cinema-img" style={img_1}>
-                <HeaderCinema active="2" />
+            <HeaderCinema active="2" />
                 <div
                 className="modal fade"
                 id="exampleModalCenter"
@@ -67,7 +75,6 @@ const AfficheCinema = ({ data }) =>  {
                 </div>
                 </div>
 
-                {data.allStrapiArticle.edges.map(document => (
                 <div className="row affiche">
                 <div className="col-7">
                     <div className="jumbotron jumbotron-fluid jumbotron-custom">
@@ -104,13 +111,103 @@ const AfficheCinema = ({ data }) =>  {
                     <img
                     className="rounded mx-auto d-block cinema-affiche"
                     alt="Affiche"
-                    src={document.node.Affiche}
+                    src={document.node.Affiche.publicURL}
                     />
                 </div>
                 </div>
-                ))}
             </div>
             </div>
+             );
+            }else{
+              
+              var imgUrl_1 = document.node.Affiche.publicURL;
+
+              const img_1 = {
+                backgroundImage: 'url("' + imgUrl_1 + '")'
+              };
+
+              return(
+                <div className="background-cinema" style={background_gradient}>
+                <div className="background-cinema-img" style={img_1}>
+                    <div
+                    className="modal fade"
+                    id="exampleModalCenter"
+                    tabIndex="-1"
+                    role="dialog"
+                    aria-labelledby="exampleModalCenterTitle"
+                    aria-hidden="true"
+                    >
+                    <div className="modal-dialog modal-video" role="document">
+                        <div className="modal-content">
+                        <div className="modal-header">
+                            <button
+                            type="button"
+                            className="close"
+                            data-dismiss="modal"
+                            aria-label="Close"
+                            >
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <iframe
+                            src="https://www.youtube-nocookie.com/embed/pHgwf2eMFnA"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                            title="Trailer Star Wars IX"
+                            className="video"
+                            />
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+    
+                    <div className="row affiche">
+                    <div className="col-7">
+                        <div className="jumbotron jumbotron-fluid jumbotron-custom">
+                        <div className="container">
+                            <h1 className="display-4">
+                            {document.node.Titre}
+                            </h1>
+                            <p className="lead jumbotron-text">
+                            {document.node.Description}
+                            </p>
+    
+                            <button
+                            type="button"
+                            className="btn btn-primary btn-primary-custom"
+                            >
+                            En Savoir Plus
+                            </button>
+                            <button
+                            type="button"
+                            className="btn btn-secondary btn-secondary-custom"
+                            data-toggle="modal"
+                            data-target="#exampleModalCenter"
+                            >
+                            Le Trailer
+                            </button>
+    
+                            <p className="jumbotron-description">
+                            {document.node.Description2}
+                            </p>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="col-5">
+                        <img
+                        className="rounded mx-auto d-block cinema-affiche"
+                        alt="Affiche"
+                        src={document.node.Affiche.publicURL}
+                        />
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+                );
+           }
+           })}
         </div>
     </Layout>
   )
@@ -134,7 +231,9 @@ export const query = graphql`
           Description
           Description2
           Lien
-          Affiche
+          Affiche{
+            publicURL
+          }
         }
       }
     }
